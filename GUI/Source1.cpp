663a7c8d063f9044a05f2606f5564eb4e06e1dc9
Roba85 <roba.ahmed211@gmail.com>
2024-04-17 18:12:30 +0200

    Added blur

--- GUI/Source1.cpp
+++ GUI/Source1.cpp
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:f1c43e8e6ed48d94b441dbcbdb34a6a22451ae067f61e03b14110baab3909450
-size 10899
+oid sha256:7d5158d5805134cea2fb4912ad1c8dbe22b209d05a573bf3773f1c61b65d5fb1
+size 12670
