7a19db849181e761c11dae813e2e81b10f81965d
Roba85 <roba.ahmed211@gmail.com>
2024-04-17 05:34:15 +0200

    Added 3 filters

--- GUI/MyForm.h
+++ GUI/MyForm.h
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:8e23ceac41db0440e663a35eb929acea62a8d4f39db6d9a6881f317738b37352
-size 51926
+oid sha256:637196edb670a9d8c6c77708b7fd4c353bdc95c2c8e2a880cbcde44c24567ac9
+size 52820
--- GUI/Source1.cpp
+++ GUI/Source1.cpp
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:e5dd73dac9a70d3a9e128f48513ec2cc302caf43412f95fe07710b28a71e891c
-size 4218
+oid sha256:d46bea9361af8ca76b23bd2bdf0e15b1b07b290d7e371f275da6f17ef7be2199
+size 6674
